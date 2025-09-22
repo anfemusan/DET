80fc3d475ee60fc01651c94c949455e6e945c3de
Andrés Felipe Muñoz <andresm-lds@hotmail.com>
2025-09-22 16:33:29 -0500

    Policy Cloud Armor

--- main.tf
+++ main.tf
@@ -103,3 +103,89 @@ resource "google_project_iam_custom_role" "cloudrun_admin" {
     "run.services.list"
   ]
 }
+
+# -----------------------------
+# 1. Cloud Armor Policy
+# -----------------------------
+resource "google_compute_security_policy" "block_ip_policy" {
+  name = "block-ip-policy"
+
+  # Bloquear IP específica
+  rule {
+    description  = "Bloquea IP específica"
+    priority     = 1000
+    action       = "deny(403)"
+
+    match {
+      versioned_expr = "SRC_IPS_V1"
+      config {
+        src_ip_ranges = ["181.51.33.25/32"]
+      }
+    }
+  }
+
+  # Regla por defecto: permitir todo
+  rule {
+    action   = "allow"
+    priority = 2147483647
+
+    match {
+      versioned_expr = "SRC_IPS_V1"
+      config {
+        src_ip_ranges = ["*"]
+      }
+    }
+  }
+}
+
+# -----------------------------
+# 2. NEG apuntando a Cloud Run
+# -----------------------------
+resource "google_compute_region_network_endpoint_group" "cloud_run_neg" {
+  name                  = "cloud-run-neg"
+  region                = "us-central1"
+  network_endpoint_type = "SERVERLESS"
+
+  cloud_run {
+    service = "web-app"
+  }
+}
+
+# -----------------------------
+# 3. Backend Service con política
+# -----------------------------
+resource "google_compute_backend_service" "cloud_run_backend" {
+  name            = "cloud-run-backend"
+  protocol        = "HTTP"
+  timeout_sec     = 30
+  security_policy = google_compute_security_policy.block_ip_policy.id
+
+  backend {
+    group = google_compute_region_network_endpoint_group.cloud_run_neg.id
+  }
+}
+
+# -----------------------------
+# 4. URL Map
+# -----------------------------
+resource "google_compute_url_map" "default" {
+  name            = "web-app-url-map"
+  default_service = google_compute_backend_service.cloud_run_backend.id
+}
+
+# -----------------------------
+# 5. Proxy
+# -----------------------------
+resource "google_compute_target_http_proxy" "default" {
+  name   = "web-app-proxy"
+  url_map = google_compute_url_map.default.id
+}
+
+# -----------------------------
+# 6. Global Forwarding Rule
+# -----------------------------
+resource "google_compute_global_forwarding_rule" "default" {
+  name       = "web-app-forwarding-rule"
+  target     = google_compute_target_http_proxy.default.id
+  port_range = "80"
+}
